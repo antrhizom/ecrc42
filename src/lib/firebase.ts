4e960260b73d92aa0d622493b8b1451ad138e902
antrhizom <151164845+antrhizom@users.noreply.github.com>
2025-12-21 14:41:43 +0100

    Update Firebase configuration with new credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase-Konfiguration - BITTE MIT DEINEN EIGENEN WERTEN ERSETZEN
 const firebaseConfig = {
-  apiKey: "DEINE_API_KEY",
-  authDomain: "DEIN_AUTH_DOMAIN",
-  projectId: "DEINE_PROJECT_ID",
-  storageBucket: "DEINE_STORAGE_BUCKET",
-  messagingSenderId: "DEINE_MESSAGING_SENDER_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyCr1L8Dhcf1dMNkoPJxB9ZoBYsKsSQKkRU",
+  authDomain: "ecrc42.firebaseapp.com",
+  projectId: "ecrc42",
+  storageBucket: "ecrc42.firebasestorage.app",
+  messagingSenderId: "783331523622",
+  appId: "1:783331523622:web:7fc78af1f01047759b3ae7"
 };
 
 // Firebase nur einmal initialisieren
